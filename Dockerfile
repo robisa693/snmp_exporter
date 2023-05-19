a425e4ca629f4cca32c8b4a0a0f7603756f79544
Roberto <roberto_isaac@live.se>
2023-05-19 22:14:06 +0200

    V.1.0.0 Working version without cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,12 @@
-#in progress
+# syntax=docker/dockerfile:1
+
+FROM python:3.8-slim-buster
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+COPY config.ini config.ini
+
+CMD ["python3", "snmp_exporter.py"]
--- snmp_exporter.py
+++ snmp_exporter.py
@@ -7,6 +7,7 @@ import configparser
 import os
 import sys
 import time
+from pysnmp.hlapi import *
 
 class ConfigParser():
 ##
@@ -42,16 +43,64 @@ class SNMPCollector(object):
     def __init__(self) -> None:
         self.c = ConfigParser()
 
-
-
+    def collect(self):
+        
+        #print("test123") 
+        old_metric_name = ''
+        for device in self.c.targets:
+            success = False
+            
+            for key in device:
+                values = key.split("-") # ['SNMP', 'public', 'OID', '1.3.6.1.4.1.1452.1.2.5.1.3.21.1.4.7', 'fw_cpu_total_util', 'description', 'sekiifw1']
+                print(values)
+                ip = device[key]
+                community = values[1]
+                oid = values[3]
+                name = values[4]
+                description = values[5]
+                hostname = values[6]
+                print(f'{old_metric_name} + {name}')
+                #if old_metric_name != name:
+                g_metric = GaugeMetricFamily(name, description, labels=['instance'])
+                old_metric_name = name
+                for (errorIndication,
+                    errorStatus,
+                    errorIndex,
+                    varBinds) in getCmd(SnmpEngine(),
+                                        CommunityData(community),
+                                        UdpTransportTarget((ip, 161)),
+                                        ContextData(),
+                                        ObjectType(ObjectIdentity(oid)),
+                                        lookupMib=False,
+                                        lexicographicMode=False):
+                    if errorIndication:
+                        print('test')
+                        print(errorIndication, file=sys.stderr)
+                        
+                    elif errorStatus:
+                        print('%s at %s' % (errorStatus.prettyPrint(),
+                                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'), file=sys.stderr)
+                        
+                    else:
+                        for varBind in varBinds:
+                            k, v = varBind
+                            print('%s = %s' % varBind)
+                            success = True
+                    if success:
+                        g_metric.add_metric(labels=[hostname],value=v)
+                    else: 
+                        print(f'could not get snmp for: {device}')
+                        #comment out this line when not testing
+                        g_metric.add_metric(labels=[f'DEBUG_failed_for_{hostname}'],value=0)
+            yield g_metric
+            #k is the OID
+            #v is the value of that OID, can be 5 for exmaple
+        
+    #    print(varBinds)
 
 if __name__ == '__main__':
-
-    ### test of configparser ###
-    c = ConfigParser()
-    ### test of configparser ###
     registry = CollectorRegistry()
-    registry.register(SNMPCollector)
+    registry.register(SNMPCollector())
     start_http_server(8000, registry=registry)
     while True:
-        time.sleep(1)
+       time.sleep(1)
